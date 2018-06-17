5e1c744c8c86fdc65bd9e8dddf161f165329809d
Sayan Faraz <sayan96@hotmail.com>
2018-06-17 17:54:30 -0400

    hacky way to get text-area to work...

--- src/app/journal-input/journal-input.component.ts
+++ src/app/journal-input/journal-input.component.ts
@@ -7,15 +7,36 @@ import { Component, OnInit, SimpleChanges } from '@angular/core';
 })
 export class JournalInputComponent implements OnInit {
 
-    private journal: String;
+    journalInput: String;
 
-    constructor() { }
+    constructor() {
+     }
 
     ngOnInit() {
     }
 
-    ngOnChanges(changes: SimpleChanges) {
+    // ngOnChanges(changes: SimpleChanges) {
+    //     // let shuffledJournal: String[] = this.shuffle(this.journalInput.split(" "));
+    //     // this.journalOutput = shuffledJournal.join(" ");
+    //     // this.journalOutput = this.journalInput;
+    //     console.log("Change detected!")
+    // }
 
+    shuffle(strArr: String[]): String[] {
+        let retArr: String[];
+        let boundary: number = strArr.length;
+        let randId: number = 0;
+        let randStr: String;
+
+        while (boundary != 0) {
+            randId = Math.floor(Math.random() * boundary);
+
+            randStr = strArr[randId]
+            strArr[randId] = strArr[boundary];
+            strArr[boundary] = randStr;
+
+            retArr.push(randStr)
+        }
+        return retArr;
     }
-
 }
